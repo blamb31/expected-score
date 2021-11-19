30636744bcdf566d50fd676a4114a385cd6fa818
blamb31 <blake.lamb31@gmail.com>
2021-11-19 15:43:28 -0700

    added .env file

--- getGameInfo.js
+++ getGameInfo.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
+const API_KEY = process.env.API_KEY
 
 const getGameInfo = async (gameId) => {
-    const req = await axios.get(`https://api.sportradar.us/nba/trial/v7/en/games/${gameId}/pbp.json?api_key=kzhs3rtx6vj99sfjhmf5puxs`)
+    const req = await axios.get(`https://api.sportradar.us/nba/trial/v7/en/games/${gameId}/pbp.json?api_key=${API_KEY}`)
     return req.data
 }
 
